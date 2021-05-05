be0450048c7f344f5e66fb76c7f3866e29c1183f
APPMAN M-095 <nutchapon.han@gmail.com>
2021-05-05 21:28:57 +0700

    fix wording

--- src/component/templates/selectionTemplate/index.jsx
+++ src/component/templates/selectionTemplate/index.jsx
@@ -16,25 +16,23 @@ const Col = styled.div`
   ${props => tw`w-full h-full`}
 `;
 
-const SelectionTemplate = () => {
-  return (
-    <FullScreenContainer>
-      <Header>
-        <TitleTextBlock title="More 🚀" />
-      </Header>
-      <Grids>
-        <Col>
-          <SelectionCard src="/project.svg" link="projects" title="PROJECT" />
-        </Col>
-        <Col>
-          <SelectionCard src="/blog.svg" link="blogs" title="MY BLOG" />
-        </Col>
-        <Col>
-          <SelectionCard src="/demo.svg" link="Demos" title="MY DEMO" />
-        </Col>
-      </Grids>
-    </FullScreenContainer>
-  );
-};
+const SelectionTemplate = () => (
+  <FullScreenContainer>
+    <Header>
+      <TitleTextBlock title="More 🚀" />
+    </Header>
+    <Grids>
+      <Col>
+        <SelectionCard src="/project.svg" link="projects" title="PROJECT" />
+      </Col>
+      <Col>
+        <SelectionCard src="/blog.svg" link="blogs" title="ARTICLE" />
+      </Col>
+      <Col>
+        <SelectionCard src="/demo.svg" link="Demos" title="DEMO" />
+      </Col>
+    </Grids>
+  </FullScreenContainer>
+);
 
 export default SelectionTemplate;
